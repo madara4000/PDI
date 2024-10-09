e23062b974e1abe78b073acee08f8b49fadfca90
madara4000 <yagomartins4000@gmail.com>
2024-10-08 21:32:04 -0300

    add

---
+++ Tarefa11/erosao.m
@@ -0,0 +1,9 @@
+%Leia uma imagem em escala de ciunza 
+%Binarize a imagem utilizando a função9 im2bw (utiliza thresholds entre [0 1]);
+%Crie uma função no Matlab/Octave que:
+%receba uma imagem binarizada
+%receba uma matriz descrevendo o elemento estruturante ;
+%implemente a operação de erosão sobre a imagem  utilizando o elemento estruturante fornecido
+%Crie uma nova imagem erodida
+%retorne como resultado o nome  do arquivo gerado com a nova imagem
+function img_erodida = erosão (imagem,ES);
