756423260d2b5762837d51c69d8a16f8cb1552bc
madara4000 <yagomartins4000@gmail.com>
2024-10-10 21:05:34 -0300

    add

---
+++ Tarefa11/AberturaeFechamento.m
@@ -0,0 +1,6 @@
+
+%Utilize as funções de erosão e dilatação desenvolvidas nas atividades
+%anteriores para:
+%Crie um script que aplique a operação de abertura sobre a imagem e
+%Crie um script que aplique a operação de fechamento sobre a
+%imagem e salve em um arquivo;
